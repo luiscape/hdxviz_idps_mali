5ff6a7eb6a7ffc231f0b601e6700070162c4ad2a
Luis Capelo <luiscape@gmail.com>
2014-09-11 09:56:03 -0400

    adding total to the data

--- code/munging.R
+++ code/munging.R
@@ -15,11 +15,19 @@ names(x) <- c('name', 'date', 'value')
 # casting
 y <- dcast(x, date ~ name)
 
+# calculating totals
+for(i in 1:nrow(y)) {
+  y$Total[i] <- sum(y[i, 2:ncol(y)])
+}
+
 # making cumulative calculation
 z <- y
 for (i in 1:(nrow(z)-1)) {
   z[i + 1, 2:ncol(z)] <- z[i, 2:ncol(z)] + z[i + 1, 2:ncol(z)]
 }
+
+
+
 
 ####################################
 ####################################
